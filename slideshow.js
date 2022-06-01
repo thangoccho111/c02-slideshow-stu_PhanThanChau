367acf666016738dd0b342309e67c0c7ef2df137
thangoccho111 <62010994+thangoccho111@users.noreply.github.com>
2022-06-01 19:54:01 +0700

    Xong

--- slideshow.js
+++ slideshow.js
@@ -1,19 +1,20 @@
 let a = 1;
 function hienThiSlideKeTiep() {
   if (a === 4) {
-    alert("Đã đến cuối");
+    a = 1;
+    document.getElementById("img").src = "./anh" + [a] + ".jpg";
+    // alert("Đã đến cuối");
   } else {
     a++;
     document.getElementById("img").src = "./anh" + [a] + ".jpg";
-    console.log(a);
   }
 }
 function hienThiSlideTruocDo() {
   if (a === 1) {
-    alert("Đã đến cuối");
+    a = 4;
+    document.getElementById("img").src = "./anh" + [a] + ".jpg";
   } else {
     a--;
     document.getElementById("img").src = "./anh" + [a] + ".jpg";
-    console.log(a);
   }
 }
